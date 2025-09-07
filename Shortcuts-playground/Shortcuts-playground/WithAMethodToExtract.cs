16258e70e5d66288c422ccfc0b41bea9ebe97852
xavi ametller <xavi.ametller@gmail.com>
2025-09-08 00:34:25 +0200

    Simplified Extract Method

--- Shortcuts-playground/Shortcuts-playground/WithAMethodToExtract.cs
+++ Shortcuts-playground/Shortcuts-playground/WithAMethodToExtract.cs
@@ -1,84 +1,43 @@
+using System.Collections;
+using System.Text;
+
 namespace ShortcutsPlayground;
 
-// Extract method: Ctrl + R + M 
+// Extract method: Ctrl + r + m 
+// Split the method in 3 functions (header, body, footer)
 public class WithSomeMethodsToExtract
 {
-    public class TennisGame1
+    public class TicketPrinter
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
-
-        public TennisGame1(string player1Name, string player2Name)
+        public string Print(Ticket ticket)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
-        }
+            StringBuilder sb = new StringBuilder();
 
-        public void WonPoint(string playerName)
-        {
-            if (playerName == "player1")
-                m_score1 += 1;
-            else
-                m_score2 += 1;
-        }
+            sb.AppendLine($"From:{ticket.Seller}");
+            sb.AppendLine($"To:{ticket.Buyer}");
+            sb.AppendLine($"Date:{ticket.EmissionDate}");
 
-        public string GetScore()
-        {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            sb.AppendLine("Articles");
+            foreach (var line in Ticket.Lines)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                sb.AppendLine($"Quantity + price and line total");
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
-            return score;
+            
+            sb.AppendLine($"From:{ticket.Total}");
+            sb.AppendLine("Thanks for buying");
+            
+            return sb.ToString();
         }
+        
     }
+}
+
+public class Ticket
+{
+    public string Seller { get; set; }
+    public string Buyer { get; set; }
+    public string EmissionDate { get; set; }
+    public string Total { get; set; }
+    public static IEnumerable Lines { get; set; }
+
 }
