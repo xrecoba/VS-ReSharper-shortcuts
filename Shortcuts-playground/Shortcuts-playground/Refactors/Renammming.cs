e23cd6417a7614a0097a342aa2bbaac1ef1e40f1
xavi ametller <xavi.ametller@gmail.com>
2025-09-09 00:39:05 +0200

    Add renaming

---
+++ Shortcuts-playground/Shortcuts-playground/Refactors/Renammming.cs
@@ -0,0 +1,15 @@
+namespace ShortcutsPlayground;
+
+/* Ctrl+rr - Rename */
+// Fix the typos by renaming the class, the method, the var and the file.
+
+
+public class ThisIsAClassssss
+{
+    public string RandomPruuperty { get; set; }
+    public void MethodToRebane()
+    {
+        var aNumber = "this is a string!";
+        
+    }
+}
