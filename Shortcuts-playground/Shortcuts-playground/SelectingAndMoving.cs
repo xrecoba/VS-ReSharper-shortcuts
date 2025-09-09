2ea494ab2807c28bdccb95c106a28eb49afe06f0
xavi ametller <xavi.ametller@gmail.com>
2025-09-09 22:40:36 +0200

    Selecting and moving extended to move lines of code Added surrounding exercise

--- Shortcuts-playground/Shortcuts-playground/SelectingAndMoving.cs
+++ Shortcuts-playground/Shortcuts-playground/SelectingAndMoving.cs
@@ -2,10 +2,22 @@ using static ShortcutsPlayground.MoveMethodsInHierarchy;
 
 namespace ShortcutsPlayground;
 
-/* Ctrl+Alt+Right Arrow to extend selection, Left Arrow to shrink */
-/* Ctrl+Alt+Shift+Arrow Up/Down to move the methods in the file up or down */
-/* Ctrl+Alt+Shift+Arrow Right/Left to move the methods inside the next one or outside the current one */
+/* Ctrl+Alt+Shift+Arrow Up/Down --> Move line up or down */
+/* With the extended selection, Ctrl+Alt+Shift+Arrow Up/Down to move the methods in the file up or down */
+/* Sort these lines */
 
+public class ClassWithLinesToSort
+{
+    public void Whatever()
+    {
+        ///3
+        ///2
+        ///1
+    }
+}
+
+/* Ctrl+Alt+Right Arrow to extend selection, Left Arrow to shrink */
+/* Ctrl+Alt+Shift+Arrow Right/Left to move the methods inside the next one or outside the current one */
 // 1. Select the methods using "extend selection" and Ctrl+Alt+Shift+Arrow Up/Down to sort them alphabetically
 // 2. Move the class ThisShouldBeMovedOutside outside class MethodsToSort using Ctrl+Alt+Shift+Arrow 
 
---
+++ Shortcuts-playground/Shortcuts-playground/Surrounding.cs
@@ -0,0 +1,14 @@
+public class Surrounding
+{
+    /* Ctrl+k+s --> Surround selected code */
+    
+    /* Use surrounding to fix this function. Bonus points if you select the code extending selection */ 
+    public class AnotherClass
+    {
+        public bool isEven(int number)
+        {
+            return false;
+        }
+    }
+    
+}
